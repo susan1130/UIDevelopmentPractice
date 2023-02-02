89da99585e8e9e03dbfd67af31798cc403ed38a1
SusanDas <48977015+susan1130@users.noreply.github.com>
2023-02-02 21:08:47 +0530

    Add files via upload

---
+++ Javascript/Functions/C17.js
@@ -0,0 +1,10 @@
+//Immediately invoked function expression IIFE
+// Function is called immediately once we define them
+
+// (function f1(){
+//     console.log("F1 function expression")
+// })()
+
+(function f2(a, b){
+    console.log(a, b)
+}) (100,30)
